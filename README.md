dc517bd59dd3497f879e540858020d05724be8c1
NeeteshMaurya <neetesh010798@gmail.com>
2021-03-10 08:04:08 +0530

    flash_chat_Screen

---
+++ README.md
@@ -0,0 +1,6 @@
+<h1>Flash Chat login/Signup Screen </h1>
+
+<div>
+  <img src="a.png" width=250 style="display:inline-block"/>
+  <img src="b.png" width=250 style="display:inline-block"/>
+</div>
---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,108 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flash_chat/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatefulWidget {
+  static String id = 'chat_screen';
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _fireStore = Firestore.instance;
+  final _auth = FirebaseAuth.instance;
+  FirebaseUser loggedInUser;
+  String messageText;
+
+  @override
+  void initState() {
+    super.initState();
+
+    getCurrentUser();
+  }
+
+  void getCurrentUser() async {
+    try{
+      final user =await _auth.currentUser();
+      if (user != null){
+        loggedInUser = user;
+        // print(loggedInUser.email);
+      }
+    }catch(e){
+      print(e);
+    }
+  }
+//we are not using get.document because it only fetch data not update the data...
+  void messagesStream() async {
+    await for(var snapshot in _fireStore.collection('messages').snapshots()){
+      for(var messages in snapshot.documents){
+        print(messages.data);
+      }
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.close),
+              onPressed: () {
+                messagesStream();
+                // _auth.signOut();
+                // Navigator.pushNamed(context, LoginScreen.id);
+                //Implement logout functionality
+              }),
+        ],
+        title: Text('⚡️Chat'),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              decoration: kMessageContainerDecoration,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {
+                        messageText = value;
+                      },
+                      decoration: kMessageTextFieldDecoration,
+                    ),
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      //messageText+ loggedInUser.email
+                      _fireStore.collection('messages').add({
+                        'text': messageText,
+                        'sender': loggedInUser.email,
+                      });
+                    },
+                    child: Text(
+                      'Send',
+                      style: kSendButtonTextStyle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
